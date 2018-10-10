98ca2d993487139de79e350d7f4932064ed3bb1d
nourharidy <nourharidy@yahoo.com>
2018-10-10 03:30:37 +0200

    Bug fix

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ module.exports = function(sources, context, config) {
             context = Object.assign(context, config.context)
         }
     }
+    context.imports = [];
+    context.import = function (url) {
+        var exists = false;
+        for (var i = 0; i < this.imports.length; i++) {
+            if(this.imports[i] === url) {
+                exists = true'
+            }
+        }
+        if(!exists) {
+            this.imports.push(url);
+        }
+        return 'import "' + url + '";'
+    }
     for (source in sources) {
         if(source.endsWith('.psol')) {
             let template = underscoreTemplate(sources[source]);
-            context.imports = [];
-            context.import = function (url) {
-                this.imports.push(url);
-                return 'import "' + url + '";'
-            }
             sources[source] = template(context)
             for (var i = 0; i > context.imports.length; i++) {
                 request.get(context.imports[i], function (error, response, contract) {
